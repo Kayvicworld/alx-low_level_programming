2d43e68ac530f63f56571712cce1bfaa8f9f6c51
root <vickayode024@gmail.com>
2022-09-29 20:39:06 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,43 @@
 #include "main.h"
+/**
+ * _evaluate - evaluate function sqrt
+ * @i: interger
+ * @n: interger
+ * Return: evaluate sqrt
+ */
+
+int _evaluate(int i, int n)
+{
+	/*Evaluate function*/
+	if (n == 0 || n == 1)
+	return (n);
+
+	else if (i * i < n)
+	return (_evaluate(i + 1, n));
+
+	else if (i * i == n) /*Condiction base*/
+	return (i);
+
+	return (-1);
+
+	return (-1);
+}
 
 /**
- * _pow_recursion - Returns the value of x raised to the power of y
- * @x: the value to multiply
- * @y: the times to multiply the value
- *
- * Return: the value multiplied y times
+ * _sqrt_recursion - evaluate sqrt
+ * @n: interger
+ * Return: Sgrt_recursion
  */
-int _pow_recursion(int x, int y)
+
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
-		return (-1);
+	int i = 0;
 
-	if (y == 0)
-		return (1);
+	if (i < 0) /*If n is negative*/
+	return (-1);
 
-	return (x * _pow_recursion(x, y - 1));
+	else
+	{
+		return (_evaluate(i, n));	/*Recursive call*/
+	}
 }
